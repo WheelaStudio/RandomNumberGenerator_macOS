a92089e48f261b564cfd305d4db89bc7900a8edd
WheelaStudio <wheelastudio@gmail.com>
2021-12-11 00:03:04 +0300

    Bug fix

--- RandomNumberGenerator/MainView.swift
+++ RandomNumberGenerator/MainView.swift
@@ -16,8 +16,12 @@ struct MainView: View {
     {
         TextField(title, text: text, prompt: nil)
     }
+    private var trailingZeroResult : String {
+        get {
+            return viewModel.result == nil ? "" : viewModel.result!.forTrailingZero()
+        }
+    }
     var body: some View {
-      
         VStack{
             HStack {
                 makeInputField(LocalizedStringKey("From"), text: $viewModel.from)
@@ -30,28 +34,28 @@ struct MainView: View {
                         errorDescription = err
                         errorAlertIsShowed = true
                     } else {
-                        result = viewModel.result!.forTrailingZero()
+                        result = trailingZeroResult
                     }
                 }
                 viewModel.saveSettings()
             })
             if !result.isEmpty {
-            Text(viewModel.result == nil ? "" : NSLocalizedString("Result", comment: "") + result).contextMenu {
-                Button(action: {
-                    let pasteboard = NSPasteboard.general
-                    pasteboard.declareTypes([.string], owner: nil)
-                    pasteboard.setString(result, forType: .string)
-                }, label: {
-                    Text(LocalizedStringKey("Copy"))
-                })
+                Text(viewModel.result == nil ? "" : NSLocalizedString("Result", comment: "") + result).contextMenu {
+                    Button(action: {
+                        let pasteboard = NSPasteboard.general
+                        pasteboard.declareTypes([.string], owner: nil)
+                        pasteboard.setString(result, forType: .string)
+                    }, label: {
+                        Text(LocalizedStringKey("Copy"))
+                    })
+                }
             }
-        }
             Spacer()
         }.alert(errorDescription, isPresented: $errorAlertIsShowed) {
             Button("OK", role: .cancel) {}
         }.onAppear{
             viewModel.loadSettings()
-            result = viewModel.result!.forTrailingZero()
+            result = trailingZeroResult
         }
         
         
